c8e20a19ea65f41127c8e90440f191a35717f914
Your Name <you@example.com>
2024-09-18 02:38:54 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+//calculator web app setup
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input) {
+  display.value += input;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function calculate() {
+  try {
+    const result = eval(display.value);
+    display.value = result;
+  } catch (error) {
+    display.value = "NULNA";
+  }
+}
